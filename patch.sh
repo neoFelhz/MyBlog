2897cc6e06fc7beecf4db6e0841fd8eb10e14019
neoFelhz <neofelhz@gmail.com>
2017-10-01 12:20:12 +0800

    Patch prism plugin

--- patch.sh
+++ patch.sh
@@ -2,3 +2,4 @@ sed -i '168d' node_modules/hexo-prism-plugin/src/index.js
 sed -i '171d' node_modules/hexo-prism-plugin/srcindex.js
 sed -i '173d' node_modules/hexo-prism-plugin/src/index.js
 sed -i '174d' node_modules/hexo-prism-plugin/src/index.js
+sed -i '176d' node_modules/hexo-prism-plugin/src/index.js
