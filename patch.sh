d76605eb5517cced78053438c51c1cfd0a20dc12
neoFelhz <neofelhz@gmail.com>
2017-10-01 12:25:17 +0800

    Fix prism plugin

--- patch.sh
+++ patch.sh
@@ -1,5 +1,1 @@
-sed -i '168d' node_modules/hexo-prism-plugin/src/index.js
-sed -i '171d' node_modules/hexo-prism-plugin/srcindex.js
-sed -i '173d' node_modules/hexo-prism-plugin/src/index.js
-sed -i '174d' node_modules/hexo-prism-plugin/src/index.js
-sed -i '176d' node_modules/hexo-prism-plugin/src/index.js
+sed -i '196d' node_modules/hexo-prism-plugin/src/index.js
