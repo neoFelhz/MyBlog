6ca8e5f6211b66b8db031dec8599f3ba1a1ffd80
neoFelhz <neofelhz@gmail.com>
2017-11-02 13:17:04 +0800

    Remove some links & Update SW import

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ git config --global push.default matching
 git config --global user.email "${GitHubEMail}"
 git config --global user.name "${GitHubUser}"
 git add --all .
-git commit -m "Auto Builder of ${GitHubUser}'s Blog"
+git commit -m "Auto builder by Travis CI: %Y-%m-%d %H:%M:%S"
 git push --quiet --force https://${GitHubKEY}@github.com/${GitHubUser}/${GitHubRepo}.git
