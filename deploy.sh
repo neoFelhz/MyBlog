7a3ea4e763172de6790f76b007b248dd0e2ff418
寂听 <jiting@jtcat.com>
2017-12-10 14:19:02 +0800

    fix : site update time display and commit history

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+set -ev
+git clone -b master https://github.com/${GitHubUser}/${GitHubRepo}.git _hexo/.blog_deploy #git clone site file to _hexo/.blog_deploy
+cd _hexo/.blog_deploy 
+git checkout master
+cd ../  #back to _hexo folder
+mv .blog_deploy/.git/ ./public/ #sync git commit history
 cd ./public
-git init
-git config --global push.default matching
 git config --global user.email "${GitHubEMail}"
 git config --global user.name "${GitHubUser}"
 git add --all .
-git commit -m "Auto builder by Travis CI: %Y-%m-%d %H:%M:%S"
-git push --quiet --force https://${GitHubKEY}@github.com/${GitHubUser}/${GitHubRepo}.git
+git commit -m "Auto builder by Travis CI: `date +"%Y-%m-%d %H:%M:%S"`"
+git push --quiet --force https://${GitHubKEY}@github.com/${GitHubUser}/${GitHubRepo}.git master:master
